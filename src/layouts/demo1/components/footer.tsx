e958f3ab2b6099d05e789a65f5309b0a6c274ae4
Brian Paidamoyo Sithole <brianacyth@icloud.com>
2025-08-04 23:44:13 +0300

    Clean up superbase dependancies. Clean up footer section.

--- src/layouts/demo1/components/footer.tsx
+++ src/layouts/demo1/components/footer.tsx
@@ -1,4 +1,3 @@
-import { generalSettings } from '@/config/general.config';
 import { Container } from '@/components/common/container';
 
 export function Footer() {
@@ -11,47 +10,18 @@ export function Footer() {
           <div className="flex order-2 md:order-1  gap-2 font-normal text-sm">
             <span className="text-muted-foreground">{currentYear} &copy;</span>
             <a
-              href="https://keenthemes.com"
+              href="https://iambriansith.com"
               target="_blank"
               className="text-secondary-foreground hover:text-primary"
             >
-              Keenthemes Inc.
+              iambriansith.com
             </a>
           </div>
           <nav className="flex order-1 md:order-2 gap-4 font-normal text-sm text-muted-foreground">
-            <a
-              href={generalSettings.docsLink}
-              target="_blank"
-              className="hover:text-primary"
-            >
-              Docs
-            </a>
-            <a
-              href={generalSettings.purchaseLink}
-              target="_blank"
-              className="hover:text-primary"
-            >
-              Purchase
-            </a>
-            <a
-              href={generalSettings.faqLink}
-              target="_blank"
-              className="hover:text-primary"
-            >
+            <a href={'#'} target="_blank" className="hover:text-primary">
               FAQ
             </a>
-            <a
-              href="https://devs.keenthemes.com"
-              target="_blank"
-              className="hover:text-primary"
-            >
-              Support
-            </a>
-            <a
-              href={generalSettings.licenseLink}
-              target="_blank"
-              className="hover:text-primary"
-            >
+            <a href={'#'} target="_blank" className="hover:text-primary">
               License
             </a>
           </nav>
