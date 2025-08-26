4818be7df368614704eb66224e636ffe3cde2625
Brian Paidamoyo Sithole <brianacyth@icloud.com>
2025-08-26 15:17:28 +0300

    Improvements on invitation acceptance form.

--- src/pages/user/my-connections/invite-respond/invite-respond-sheet.tsx
+++ src/pages/user/my-connections/invite-respond/invite-respond-sheet.tsx
@@ -1,14 +1,27 @@
 import { useEffect, useState } from 'react';
 import { API_URL } from '@/auth/adapters/jwt-auth-adapter.ts';
-import { Rating } from '@/pages/store-client/components/sheets/product-details-sheet.tsx';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Empty, Result, Spin } from 'antd';
 import axios from 'axios';
-import { CheckCheckIcon, UserCheck, UserX } from 'lucide-react';
+import { UserCheck, UserX } from 'lucide-react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { LifeStatus } from '@/lib/enums';
 import { toAbsoluteUrl } from '@/lib/helpers.ts';
 import { Any } from '@/lib/interfaces';
-import { Badge } from '@/components/ui/badge.tsx';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent } from '@/components/ui/card.tsx';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form.tsx';
+import { Input } from '@/components/ui/input.tsx';
+import { ScrollArea } from '@/components/ui/scroll-area.tsx';
+import { Separator } from '@/components/ui/separator.tsx';
 import {
   Sheet,
   SheetBody,
@@ -34,15 +47,32 @@ const mappedData: Any = {
   otherGivenNames: 'Other names',
 };
 
+const getInviteResponseSchema = () => {
+  return z
+    .object({
+      password: z.string().min(1, { message: 'Password is required.' }),
+      passwordConfirm: z.string().min(1, { message: 'Confirm Password' }),
+    })
+    .refine((data) => data.password === data.passwordConfirm, {
+      message: 'Passwords are not matching',
+      path: ['passwordConfirm'],
+    });
+};
+
+export type InviteResponseSchema = z.infer<
+  ReturnType<typeof getInviteResponseSchema>
+>;
+
 const setupPersonSummary = (person: Any): Any[] => {
   const data: Any = [];
   const summaryProps = Object.keys(mappedData);
   Object.keys(person)
     .filter((key) => summaryProps.includes(key))
     .map((prop: string) => {
+      const isArray = Array.isArray(person[prop]);
       data.push({
         text: mappedData[prop],
-        info: person[prop],
+        info: isArray ? person[prop][0] : person[prop],
       });
     });
 
@@ -61,6 +91,14 @@ export function InviteRespondSheet({
   const [invitation, setInvitation] = useState<any | null>(null);
   const [isProcessing, setIsProcessing] = useState<boolean>(false);
   const [items, setItems] = useState<Any>([]);
+
+  const form = useForm<InviteResponseSchema>({
+    resolver: zodResolver(getInviteResponseSchema()),
+    defaultValues: {
+      password: '',
+      passwordConfirm: '',
+    },
+  });
 
   useEffect(() => {
     const fetchInvite = async () => {
@@ -89,7 +127,31 @@ export function InviteRespondSheet({
     };
 
     fetchInvite();
-  }, [token]);
+  }, []);
+
+  async function onSubmit(values: InviteResponseSchema) {
+    try {
+      setIsProcessing(true);
+      setError(null);
+
+      const dto: Any = {
+        token: token,
+        password: values.password as LifeStatus,
+      };
+
+      await axios.post(`${API_URL}/invitations/accept`, dto);
+      onAccept();
+    } catch (err) {
+      console.error('[ADD_PERSON_SHEET] Error adding person:', err);
+      setError(
+        err instanceof Error
+          ? err.message
+          : 'An unexpected error occurred. Please try again.',
+      );
+    } finally {
+      setIsProcessing(false);
+    }
+  }
 
   if (loading) {
     return (
@@ -133,53 +195,114 @@ export function InviteRespondSheet({
         <SheetHeader className="border-b py-3.5 px-5 border-border">
           <SheetTitle>Invitation</SheetTitle>
         </SheetHeader>
-        <SheetBody className="w-full flexjustify-center grow">
-          <CardContent className="flex flex-col space-y-3 p-3">
-            <Card className="relative items-center justify-center white-100 mb-6.5">
-              <img
-                src={toAbsoluteUrl('/media/avatars/blank.png')}
-                className="size-50"
-                alt="image"
-              />
-            </Card>
-
-            <div className="flex flex-col gap-2.5 lg:mb-5">
-              {items.map((item: Any, index: number) => (
-                <div className="flex items-center gap-2.5" key={index}>
-                  <span className="text-sm font-light text-foreground min-w-14 xl:min-w-24 shrink-0">
-                    {item?.text}
-                  </span>
-                  <span className="text-sm font-medium text-foreground min-w-14 xl:min-w-24 shrink-0">
-                    {item?.info}
-                  </span>
-                </div>
-              ))}
-            </div>
-
-            <span className="text-sm font-normal text-foreground block mb-7">
-              {invitation.sourcePerson.firstName}{' '}
-              {invitation.sourcePerson.lastName} added you as {invitation.type}{' '}
-              in the family tree. Do you wish to accept this invitation?
-            </span>
-          </CardContent>
-        </SheetBody>
-        <SheetFooter className="border-t py-3.5 px-5 border-border flex gap-2">
-          <Button className="grow" disabled={isProcessing} onClick={onAccept}>
-            <span className="flex items-center gap-2">
-              <UserCheck className="h-4 w-4" /> Accept Invite
-            </span>
-          </Button>
-
-          <Button
-            className="grow bg-red-500 hover:bg-red-600 text-white"
-            disabled={isProcessing}
-            onClick={onReject}
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="block w-full space-y-5"
           >
-            <span className="flex items-center gap-2">
-              <UserX className="h-4 w-4" /> Reject Invite
-            </span>
-          </Button>
-        </SheetFooter>
+            <SheetBody className="px-5 py-0">
+              <ScrollArea className="h-[calc(100dvh-11.75rem)] pe-3 -me-3">
+                <CardContent className="flex flex-col space-y-3 p-3">
+                  <Card className="flex items-center justify-center white-100 mb-4">
+                    <img
+                      src={toAbsoluteUrl('/media/avatars/blank.png')}
+                      className="size-40"
+                      alt="image"
+                    />
+                  </Card>
+
+                  <div className="flex flex-col gap-2.5 lg:mb-5">
+                    {items.map((item: Any, index: number) => (
+                      <div className="flex items-center gap-2.5" key={index}>
+                        <span className="text-sm font-light text-foreground min-w-14 xl:min-w-24 shrink-0">
+                          {item?.text}
+                        </span>
+                        <span className="text-sm font-medium text-foreground min-w-14 xl:min-w-24 shrink-0">
+                          {item?.info}
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+
+                  <span className="text-sm font-normal text-foreground block mb-3">
+                    {invitation.sourcePerson.firstName}{' '}
+                    {invitation.sourcePerson.lastName} added you as{' '}
+                    {invitation.type} in the family tree. Do you wish to accept
+                    this invitation?
+                    <div className="mt-3">
+                      {!invitation.targetPerson?.user?.id
+                        ? 'Accepting this invitation will allow you login and extend the family tree. Set your password below'
+                        : null}
+                    </div>
+                  </span>
+                  {!invitation.targetPerson?.user?.id && (
+                    <div>
+                      <Separator className="mb-3"></Separator>
+                      <div className="flex gap-2">
+                        <div className="grow">
+                          <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                  <Input
+                                    type="password"
+                                    placeholder="Password"
+                                    {...field}
+                                  />
+                                </FormControl>
+                                <FormMessage />
+                              </FormItem>
+                            )}
+                          />
+                        </div>
+
+                        <div className="grow">
+                          <FormField
+                            control={form.control}
+                            name="passwordConfirm"
+                            render={({ field }) => (
+                              <FormItem>
+                                <FormLabel>Confirm Password</FormLabel>
+                                <FormControl>
+                                  <Input
+                                    type="password"
+                                    placeholder="confirmPassword"
+                                    {...field}
+                                  />
+                                </FormControl>
+                                <FormMessage />
+                              </FormItem>
+                            )}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </CardContent>
+              </ScrollArea>
+            </SheetBody>
+            <SheetFooter className="border-t py-3.5 px-5 border-border flex gap-2">
+              <Button className="grow" disabled={isProcessing} type="submit">
+                <span className="flex items-center gap-2">
+                  <UserCheck className="h-4 w-4" /> Accept Invite
+                </span>
+              </Button>
+
+              <Button
+                className="grow bg-red-500 hover:bg-red-600 text-white"
+                disabled={isProcessing}
+                onClick={onReject}
+              >
+                <span className="flex items-center gap-2">
+                  <UserX className="h-4 w-4" /> Reject Invite
+                </span>
+              </Button>
+            </SheetFooter>
+          </form>
+        </Form>
       </SheetContent>
     </Sheet>
   );
